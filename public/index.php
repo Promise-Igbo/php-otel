09adc6def3706bff437325ec3a55e7c191b1c982
Promise Igbo <100773978+Promise-Igbo@users.noreply.github.com>
2026-01-29 13:15:05 +0100

    Add OpenTelemetry logging to index.php
    Initialize OpenTelemetry Logger and log application boot.

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
+use Monolog\Logger;
+use OpenTelemetry\API\Globals;
+use OpenTelemetry\Contrib\Logs\Monolog\Handler;
 
 define('LARAVEL_START', microtime(true));
 
@@ -9,11 +12,6 @@ define('LARAVEL_START', microtime(true));
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
 */
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
@@ -24,24 +22,34 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |--------------------------------------------------------------------------
 | Register The Auto Loader
 |--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
 */
 
 require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
-| Run The Application
+| Initialize OpenTelemetry Logger (SAFE)
 |--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
+*/
+
+$loggerProvider = Globals::loggerProvider();
+
+$otelHandler = new Handler(
+    $loggerProvider,
+    Logger::INFO,
+    true
+);
+
+$otelLogger = new Logger('php-otel');
+$otelLogger->pushHandler($otelHandler);
+
+// Optional: log application boot
+$otelLogger->info('Laravel application bootstrapping');
+
+/*
+|--------------------------------------------------------------------------
+| Bootstrap The Laravel Application
+|--------------------------------------------------------------------------
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
